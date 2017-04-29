72d4508eae7b36011c516b606366c46edeea772f
Ben Pig Chu <benpichu@gmail.com>
2017-04-29 14:36:27 +0800

    render framework test

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,7 +1,21 @@
+#include <cstddef>
 #include "Camera.h"
 #include "Bitmap.h"
 #include "Scene.h"
 
 void Camera::render(Bitmap& bitmap,const Scene& scene)noexcept{
-
+	size_t height=bitmap.getHeight();
+	size_t width=bitmap.getWidth();
+	for(int i=0;i<height;i++){
+		for(int j=0;j<width;j++){
+			float x=i+0.5-width/2.0;
+			float y=j+0.5-height/2.0;
+			auto result=scene.testIntersect(this->makeRay(x,y));
+			if(result.second.isIntersect){
+				bitmap.at(i,j)=Vector3(10,10,10);
+			}else{
+				bitmap.at(i,j)=Vector3(0,0,0);
+			}
+		}
+	}
 }
--- src/headers/Bitmap.h
+++ src/headers/Bitmap.h
@@ -12,10 +12,16 @@ class Bitmap{
 	public:
 	Bitmap(size_t height,size_t width)noexcept:height(height),width(width),data(::std::vector<Vector3>(height*width)){}
 	inline const Vector3& at(ptrdiff_t x,ptrdiff_t y)const{
-		if(x<0||x>=height||y<0||y>=width){
+		if(x<0||x>=width||y<0||y>=height){
 			throw ::std::out_of_range("Bitmap element access out of range.");
 		}
 		return this->data[x+y*this->width];
+	}
+	inline size_t getHeight()noexcept{
+		return this->height;
+	}
+	inline size_t getWidth()noexcept{
+		return this->width;
 	}
 	inline Vector3& at(ptrdiff_t x,ptrdiff_t y){
 		if(x<0||x>=height||y<0||y>=width){
