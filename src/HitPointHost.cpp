299b9aa90906c5f8557ddef523bd35c13d0bab58
Ben Pig Chu <benpichu@gmail.com>
2017-06-22 15:29:47 +0800

    hitpoint design

---
+++ src/HitPointHost.cpp
@@ -0,0 +1,57 @@
+#include <cstddef>
+#include "Vector3.h"
+#include "KDTree.hpp"
+#include "HitPointHost.h"
+#include "AABB.h"
+
+AABB HitPointHost::getAABB(HitPoint hp)const noexcept{
+	return AABB(hp.position,hp.position);
+}
+
+void HitPointHost::processNode(Node& node)const noexcept{
+	if(node.datas.size()<=3){
+		return;
+	}else{
+		Axis splitAxis=Axis::NONE;
+		double maxDiff=0;
+		if(maxDiff<=::std::abs(node.aabb.min.x-node.aabb.max.x)){
+			maxDiff=::std::abs(node.aabb.min.x-node.aabb.max.x);
+			splitAxis=Axis::X;
+		}
+		if(maxDiff<=::std::abs(node.aabb.min.y-node.aabb.max.y)){
+			maxDiff=::std::abs(node.aabb.min.y-node.aabb.max.y);
+			splitAxis=Axis::Y;
+		}
+		if(maxDiff<=::std::abs(node.aabb.min.z-node.aabb.max.z)){
+			maxDiff=::std::abs(node.aabb.min.z-node.aabb.max.z);
+			splitAxis=Axis::Z;
+		}
+		auto comparer=[splitAxis](HitPoint hp1,HitPoint hp2){
+			switch(splitAxis){
+			case Axis::X:
+				return hp1.position.x<hp2.position.x;
+				break;
+			case Axis::Y:
+				return hp1.position.y<hp2.position.y;
+				break;
+			case Axis::Z:
+				return hp1.position.z<hp2.position.z;
+				break;
+			default:
+				return true;
+			}
+		};
+		//sort by pos
+		::std::sort(node.datas.begin(),node.datas.end(),comparer);
+		//split at middle
+		::std::vector<HitPoint> l,r;
+		::std::copy(node.datas.begin(),node.datas.begin()+node.datas.size()/2,::std::back_inserter(l));
+		::std::copy(node.datas.begin()+node.datas.size()/2,node.datas.end(),::std::back_inserter(r));
+		//make subnode
+		node.left=this->makeNode(l);
+		node.right=this->makeNode(r);
+		//return
+		node.datas.clear();
+	}
+}
+
--- src/headers/HitPointHost.h
+++ src/headers/HitPointHost.h
@@ -1,15 +1,26 @@
 #ifndef HITPOINTHOST_H
 #define HITPOINTHOST_H
-#include<cstddef>
+#include <cstddef>
+#include <functional>
 #include "Vector3.h"
+#include "AABB.h"
+#include "KDTree.hpp"
 
 struct HitPoint{
 	Vector3 position;
 	Vector3 normal;
 	Vector3 rayDirection;
-	Vector3 rayDirection;
 	ptrdiff_t imageX,imageY;
 	Vector3 weight;
+	double radius;
+	size_t photonCount;
+	size_t newPhotonCount;
+};
+
+class HitPointHost:public KDTree<HitPoint>{
+	protected:
+	AABB getAABB(HitPoint d)const noexcept override;
+	void processNode(Node& node)const noexcept override;
 };
 
 
